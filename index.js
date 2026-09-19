ce66abb6d938c5b01f1b6e0fec82df3861de86a7
JuanXRossi <juancruzrossi3009@gmail.com>
2026-09-19 18:35:52 -0300

    Add CORS config

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const backend = require("i18next-fs-backend");
 const middleware = require("i18next-http-middleware");
 const mongoose = require("mongoose");
 require("dotenv").config();
+const cors = require("cors");
 
 i18next
   .use(backend)
@@ -15,6 +16,12 @@ const port = process.env.PORT;
 const api = process.env.API;
 
 app.use(middleware.handle(i18next));
+app.use(cors({
+    origin: ["http://localhost:3000", "https://mydomain.com"],
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    credentials: true,
+    allowedHeaders: ["Content-Type", "Authorization", "Accept-Language"]
+}));
 
 app.get(`${api}/health`, (req, res) => {
   res.send(req.t("validationFailed"));
