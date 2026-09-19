eae010c7d215ee035e33a3a017b7da59dab4ec24
JuanXRossi <juancruzrossi3009@gmail.com>
2026-09-19 18:13:47 -0300

    First commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const i18next = require("i18next");
+const backend = require("i18next-fs-backend");
+const middleware = require("i18next-http-middleware");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+i18next
+  .use(backend)
+  .use(middleware.LanguageDetector)
+  .init({ fallbackLng: "en", backend: { loadPath: "locales/{{lng}}.json" } });
+
+const app = express();
+const port = process.env.PORT;
+const api = process.env.API;
+
+app.use(middleware.handle(i18next));
+
+app.get(`${api}/health`, (req, res) => {
+  res.send(req.t("validationFailed"));
+});
+
+mongoose
+  .connect(process.env.CONNECTION_STRING)
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch((error) => console.log(error));
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
