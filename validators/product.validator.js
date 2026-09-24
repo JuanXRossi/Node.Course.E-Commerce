ce46ab46c8792fec63094f8c796aa50d5600e6b6
JuanXRossi <juancruzrossi3009@gmail.com>
2026-09-24 14:46:42 -0300

    Delete static messages in product validator

--- validators/product.validator.js
+++ validators/product.validator.js
@@ -3,65 +3,35 @@ import { body, validationResult } from "express-validator";
 export const createProductValidation = [
   body("title")
     .notEmpty()
-    .withMessage(
-      (value, { req }) =>
-        req.t("productTitleRequired"),
-    )
+    .withMessage((value, { req }) => req.t("productTitleRequired"))
     .isLength({ min: 2, max: 10 })
-    .withMessage(
-      (value, { req }) =>
-        req.t("productTitleLength"),
-    )
+    .withMessage((value, { req }) => req.t("productTitleLength"))
     .trim(),
   body("category")
     .notEmpty()
-    .withMessage(
-      (value, { req }) => req.t("categoryRequired"),
-    )
+    .withMessage((value, { req }) => req.t("categoryRequired"))
     .isMongoId()
-    .withMessage(
-      (value, { req }) => req.t("invalidCategoryId"),
-    ),
+    .withMessage((value, { req }) => req.t("invalidCategoryId")),
   body("price")
     .notEmpty()
-    .withMessage(
-      (value, { req }) => req.t("priceRequired"),
-    )
+    .withMessage((value, { req }) => req.t("priceRequired"))
     .isFloat({ min: 0 })
-    .withMessage(
-      (value, { req }) =>
-        req.t("pricePositive"),
-    ),
+    .withMessage((value, { req }) => req.t("pricePositive")),
   body("description")
     .notEmpty()
-    .withMessage(
-      (value, { req }) =>
-        req.t("descriptionRequired"),
-    )
+    .withMessage((value, { req }) => req.t("descriptionRequired"))
     .isLength({ min: 5, max: 1000 })
-    .withMessage(
-      (value, { req }) =>
-        req.t("descriptionLength"),
-    )
+    .withMessage((value, { req }) => req.t("descriptionLength"))
     .trim(),
   body("countInStock")
     .notEmpty()
-    .withMessage(
-      (value, { req }) =>
-        req.t("stockCountRequired"),
-    )
+    .withMessage((value, { req }) => req.t("stockCountRequired"))
     .isInt({ min: 0, max: 99999 })
-    .withMessage(
-      (value, { req }) =>
-        req.t("stockCountRange"),
-    ),
+    .withMessage((value, { req }) => req.t("stockCountRange")),
   body("rating.count")
     .optional()
     .isInt({ min: 0 })
-    .withMessage(
-      (value, { req }) =>
-        req.t("ratingCountPositive"),
-    ),
+    .withMessage((value, { req }) => req.t("ratingCountPositive")),
 ];
 
 export const handleValidationErrors = (req, res, next) => {
