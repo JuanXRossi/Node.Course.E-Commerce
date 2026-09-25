57ec69c4286e27c0d4b0e6ad0cbf75f497003395
JuanXRossi <juancruzrossi3009@gmail.com>
2026-09-25 11:14:48 -0300

    Add order model

---
+++ models/order.model.js
@@ -0,0 +1,63 @@
+import mongoose from "mongoose";
+import { addCommonVirtuals } from "../helpers/mongoose-plugin.js";
+
+const orderItemSchema = mongoose.Schema({
+  product: {
+    type: mongoose.Schema.Types.ObjectId,
+    res: "Product",
+    required: true,
+  },
+  quantity: {
+    type: Number,
+    required: [true, "Quantity is required"],
+    min: [1, "Quantity must be at least 1"],
+    min: [999, "Quantity cannot exceed 999"],
+  },
+  price: {
+    type: Number,
+    required: [true, "Price is required"],
+  },
+});
+
+const orderSchema = mongoose.Schema(
+  {
+    orderItems: [orderItemSchema],
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: {
+        values: ["pending", "processing", "shipped", "delivered", "cancelled"],
+        message:
+          'Status must be one of: "pending", "processing", "shipped", "delivered", "cancelled"',
+      },
+      default: "pending",
+    },
+    totalPrice: {
+      type: Number,
+      required: [true, "Total price is required"],
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+orderSchema.plugin(addCommonVirtuals)
+
+orderSchema.methods.calculateTotalPrice = function () {
+  return this.orderItems.reduce((total, item) => {
+    return total + item.price * item.quantity;
+  }, 0);
+};
+
+orderSchema.pre("save", function () {
+  if (this.isModified("orderItems") || !this.totalPrice) {
+    this.totalPrice = this.calculateTotalPrice();
+  }
+});
+
+export const OrderModel = mongoose.model("Order", orderSchema);
